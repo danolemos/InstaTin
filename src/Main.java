2b47f38fb2abfb4530b6b4413757492fc7d7723a
danielolemos <danielolemos10@gmail.com>
2024-06-04 20:15:48 -0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public abstract class Main {
+
+    Sistema s;
+
+    Usuario u;
+
+    public int opc;
+
+    opc = public abstract int s.menu1(s, u);
+
+    while(opc != 0)
+
+    {
+        if (opc == 1) {
+            cadPessoa(s);
+        } else if (opc == 2) {
+            cadEmpresa(s);
+        } else if (opc == 3) {
+            login(s);
+        };
+    };
+}
